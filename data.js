fc97c3e3b57c495754ebda1b49de59d77f6ef2bb
JSenpa1 <jonathansusanto2015@gmail.com>
2023-04-02 13:01:47 +0700

    almost finished game

--- data.js
+++
@@ -1,7 +0,0 @@
-function saveNameAndRedirect() {
-    var petname = document.getElementById("petname").value;
-    const petUrl = document.getElementById("charImg").src;
-    localStorage.setItem("petname", petname);
-    localStorage.setItem("charImg-url", petUrl);
-    window.location.href = "home.html";
-}
